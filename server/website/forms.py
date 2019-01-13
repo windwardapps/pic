3071a7f1b23e7730e232c700c03a645041772ce1
Kyle Mulleady <kyle.mulleady@gmail.com>
2019-01-12 20:53:16 -1000

    login css

--- server/website/forms.py
+++ server/website/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class AuthForm(forms.Form):
-    email = forms.EmailField()
+    email = forms.EmailField(widget=forms.TextInput(attrs={'autofocus': True}))
     password = forms.CharField(widget=forms.PasswordInput)
